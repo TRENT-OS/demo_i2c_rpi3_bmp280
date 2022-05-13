52f0ca4050153c759d81dcf68f497842c67b5d87
Florian Hauschild <florian.hauschild@hensoldt.net>
2022-05-13 13:48:26 +0200

    [DEV] SEOS-3359 Added test call to device
    * Added library to compilation system.
    * Added Functions to main test file to read out the sensor data
    * Test code reads out the sensor 20 times  to see if measurents varie slightly to see if we are talking to a real device

--- i2c_test.c
+++ i2c_test.c
@@ -6,6 +6,7 @@
 #include "OS_Error.h"
 #include "lib_debug/Debug.h"
 #include "i2c.h"
+#include "bmp280.h"
 #include <stdint.h>
 #include <stdbool.h>
 //#include "OS_Dataport.h"
@@ -17,6 +18,26 @@
 #define DEVICE (0x76 << 1)
 
 
+void getSensorData(bmp280_t* bmp280_dev)
+{
+    bool bme280p = bmp280_dev->id == BME280_CHIP_ID;
+    float pressure, temperature, humidity;
+
+    bmp280_read_float(bmp280_dev, &temperature, &pressure, &humidity);
+    Debug_LOG_INFO("Pressure: %.2f Pa, Temperature: %.2f C", pressure, temperature);
+    if (bme280p)
+    {
+        Debug_LOG_INFO("Humidity: %.2f %%", humidity);
+    }
+}
+
+void wait(void)
+{
+    for (int i = 0; i < 100000 * 2000; i++)
+    {
+        __asm__("nop");
+    }
+}
 
 OS_Error_t run(void)
 {
@@ -64,9 +85,27 @@ OS_Error_t run(void)
     }  
 
     Debug_LOG_INFO("ctrl_meas dump is 0x%x", buf[0]);
-    buf[0] = 0xf4;
+
+    bmp280_params_t params;
+
+
+    bmp280_init_default_params(&params);
+
+    bmp280_t bmp280_dev;
+    bmp280_dev.i2c_dev.bus = 0;
+    bmp280_dev.i2c_dev.addr = BMP280_I2C_ADDRESS_0;
+
+    bmp280_init(&bmp280_dev, &params);
+    
+    for(int i = 0; i < 20; i++)
+    {
+        wait();
+        getSensorData(&bmp280_dev);
+    }
+    
+    /*    buf[0] = 0xf4;
     buf[1] = 0xa7;
-    err = i2c_write(0x76, 2, &tmp, buf);
+    i2c_err = i2c_write(0x76, 2, &tmp, buf);
     if(i2c_err != I2C_SUCCESS)
     {
         Debug_LOG_ERROR("i2c_read() returned errorcode %d", i2c_err);
@@ -79,6 +118,8 @@ OS_Error_t run(void)
         Debug_LOG_WARNING("i2c_write() failed to write all data");
         return err;
     }
+*/
+
     Debug_LOG_INFO("Done");
     return err;
 }

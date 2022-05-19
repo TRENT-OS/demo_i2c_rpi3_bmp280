a8ac8287ee43c5c6101f230a07ffe6dc25e7cc20
Florian Hauschild <florian.hauschild@hensoldt.net>
2022-05-19 17:07:26 +0200

    [DEV] SEOS-3359 Updated BMP280 driver library to provide init status
    Allows a client application to wait until the sensor is initialised

--- components/bmp280/src/bmp280_rpc.c
+++ components/bmp280/src/bmp280_rpc.c
@@ -13,18 +13,30 @@ static bmp280_t bmp280_dev = {
     .i2c_dev.addr = BMP280_I2C_ADDRESS_0,
 };
 
+static bool init = false;
+
 void post_init(void)
 {
     Debug_LOG_DEBUG("[%s] %s running", get_instance_name(), __func__);
+    init = false;
     bmp280_params_t params;
     bmp280_init_default_params(&params);
-
+    i2c_wait_for_bus_initialisation(&(bmp280_dev.i2c_dev.bus));
+    Debug_LOG_INFO("[%s] I2C ready, now initialising sensor", get_instance_name());
     bmp280_init(&bmp280_dev, &params);
     Debug_LOG_INFO("bmp280 initialised");
+    init = true;
 }
 
 void bmp280_rpc_get_data(float* temperature, float* pressure, float* humidity)
 {
+    if(!init)
+    {
+        *temperature = 0.0;
+        *pressure = 0.0;
+        *humidity = 0.0;
+        return;
+    }
     bmp280_read_float(&bmp280_dev, temperature, pressure, humidity);
 
     bool bme280p = bmp280_dev.id == BME280_CHIP_ID;
@@ -32,4 +44,9 @@ void bmp280_rpc_get_data(float* temperature, float* pressure, float* humidity)
     {
         *humidity = 0.0;
     }
+}
+
+bool bmp280_rpc_sensor_ready(void)
+{
+    return init;
 }
